e31b767b9a3fe0d73cede4c6a0151d8a83ebe088
BikkuMyy <maripartan@gmail.com>
2017-11-14 23:54:23 +0200

    tehtavaa 12

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -64,6 +64,51 @@ public class Stepdefs {
     public void tearDown(){
         driver.quit();
     }
+    
+    // create new user tests
+    
+    @Given("^command new user is selected$")
+    public void new_user_selected() throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();          
+    }
+    
+    @When("^a valid username \"([^\"]*)\" and password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void valid_username_and_password_are_given(String username, String password){
+        createUser(username, password, password);
+    }
+    
+    @When("^too short username \"([^\"]*)\" and valid password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void too_short_username_and_valid_password_are_given(String username, String password) throws Throwable {
+        createUser(username, password, password);
+    }
+    
+    @When("^valid username \"([^\"]*)\" and too short password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void valid_username_and_too_short_password_are_given(String username, String password) throws Throwable {
+        createUser(username, password, password);
+    }
+    
+    @When("^taken username \"([^\"]*)\" and valid password \"([^\"]*)\" and matching password confirmation are entered$")
+    public void taken_username_and_valid_password_are_given(String username, String password) throws Throwable {
+        createUser(username, password, password);
+    }
+    
+    @When("^valid username \"([^\"]*)\" and password \"([^\"]*)\" and invalid password confirmation \"([^\"]*)\" are entered$")
+    public void valid_username_and_password_and_invalid_confirmation_are_given(String username, String password, String confirmation) throws Throwable {
+        createUser(username, password, confirmation);
+    }
+    
+    @Then("^a new user is created$")
+    public void a_new_user_is_created() throws Throwable {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+    
+    @Then("^user is not created and error \"([^\"]*)\" is reported$")
+    public void new_user_is_not_created_and_error_message_is_given(String error) throws Throwable {
+        pageHasContent(error);
+ 
+    }
         
     /* helper methods */
  
@@ -79,5 +124,17 @@ public class Stepdefs {
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
         element.submit();  
-    } 
+    }
+    
+    private void createUser(String username, String password, String confirmation) {
+        assertTrue(driver.getPageSource().contains("Give your credentials to login"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(confirmation);
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+    }
 }
